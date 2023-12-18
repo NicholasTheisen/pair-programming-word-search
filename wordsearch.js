90d07a6b2537baee89fa2b7933f87db33a68836c
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-12-18 02:14:08 +0000

    wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
+
+//Coded by Nicholas, Eva, Ina
+const transpose = m => m[0].map((x, i) => m.map(x => x[i])) // Transposes the letters in the array
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = transpose(letters).map(ls => ls.join('')) // Joins the letters in the array vertically
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
+    for (l of verticalJoin) {  // Checks if the word is in the array vertically
+        if (l.includes(word)) return true;  
+    }
+    return false;  // Returns false if the word is not found
+};
 
 module.exports = wordSearch
